3f41f1cd8c2feb19738989e893c35d18ccab51c3
ElinaUn3 <uncholeg@yandex.ru>
2024-06-03 16:03:36 +0300

    сделала третий шаг

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,8 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
+const playersGreeting = () => {
 console.log("Welcome to the Brain Games!");
 const name = readlineSync.question('May I have your name?');
 console.log(`Hello, ${name}!`);
+return name
+};
+export default playersGreeting
