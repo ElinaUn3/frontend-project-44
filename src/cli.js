806be4ce86762f596d357b7ba1e67a000b74659b
ElinaUn3 <uncholeg@yandex.ru>
2024-06-11 21:58:00 +0300

    fixed problems with the linter

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
-const playersGreeting = () => {
-console.log("Welcome to the Brain Games!");
-const name = readlineSync.question('May I have your name?');
-console.log(`Hello, ${name}!`);
-return name
+
+const userGreetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+  return playerName;
 };
-export default playersGreeting
+
+export default userGreetings;
